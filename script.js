0eca7a916a60c83e9fb07ed9b2c3188bccee22d2
Kee Rui <178910386+krsng-lab@users.noreply.github.com>
2026-05-30 15:54:05 +0800

    feat: create displayController IIFE to handle display

--- script.js
+++ script.js
@@ -52,25 +52,132 @@ const gameController = (() => {
         [0, 4, 8], [2, 4, 6]             // diagonals
     ];
 
+	let isGameOver = false;
+
 	const checkWin = () => {
-		const board = gameboard.getBoard();
-		return winConditions.some(conditions => {
-			return conditions.every(i => board[i] === activePlayer.marker);
+		const currentBoard = gameboard.getBoard();
+		return winConditions.some(conditions => { 
+			return conditions.every(i => currentBoard[i] === activePlayer.marker);
 		})
+	}
+
+	const checkDraw = () => {
+		const currentBoard = gameboard.getBoard();
+		return (!currentBoard.includes(""));
 	}
 
 	const playRound = (position) => {
 
+		// stop playing round if game is over
+		if (isGameOver) return;
+
 		const moveValid = gameboard.placeMarker(activePlayer.marker, position);
-		
+	
 		if (moveValid) {
 			if (checkWin()) {
-				console.log(`${activePlayer.name} wins!`);
+				isGameOver = true;
+				return "win"; 
+			} else if (checkDraw()) {
+				isGameOver = true;
+				return "draw";
 			} else {switchPlayer();}
-		} else { 
-			console.log("Try picking a different square.");
 		}
 	}
 
-	return { getActivePlayer, playRound };
+	const resetGame = () => {
+		isGameOver = false;
+		activePlayer = player1;
+	}
+
+	const getIsGameOver = () => isGameOver;
+
+	return { getActivePlayer, playRound , resetGame, getIsGameOver };
 })()
+
+// displayController IIFE to handle 
+const displayController = (() => {
+
+	const board = document.getElementById('gameboard-ui');
+
+	const renderBoard = () => {
+
+		// clear the board
+		board.innerHTML = '';
+
+		const currentBoard = gameboard.getBoard();
+
+		// loop through the array to create grid squares
+		currentBoard.forEach((marker, index) => { 
+
+			const square = document.createElement('div');
+
+			square.classList.add('square'); 
+
+			// store the array index directly on the element
+			square.dataset.index = index;
+
+			// display 'X', 'O' or empty string
+			square.textContent = marker;
+
+			square.addEventListener('click', squareClick);
+
+			// append new square to grid container
+			board.appendChild(square);
+		})
+	}
+
+	
+	const status = document.getElementById('status-message');
+
+	const updateStatus = (outcome) => {
+		if (outcome === "win") {
+			const activePlayer = gameController.getActivePlayer();
+			status.textContent = `${activePlayer.name} won!`	
+		} 
+		else if (outcome === "draw") {status.textContent = 'Draw!';} 
+		else {
+
+			// stop updating status if game is over
+			if (gameController.getIsGameOver()) return;
+
+			const activePlayer = gameController.getActivePlayer();
+			status.textContent = `It's ${activePlayer.name}'s turn.`;
+			}
+	}
+	
+
+	const squareClick = (e) => {
+
+		// retrieve the index from specific square clicked
+		const i = parseInt(e.target.dataset.index);
+
+		// pass the index as argument to play a round
+		const outcome = gameController.playRound(i);
+
+		// redraw the board to include new marker
+		renderBoard();
+		
+		updateStatus(outcome);
+	}
+
+	const resetBtn = document.getElementById('reset-btn');
+
+	const clearBoard = () => {
+
+	gameboard.resetBoard();
+
+	gameController.resetGame();
+
+	renderBoard();
+
+	// Reset the status message
+	updateStatus();
+	}
+
+	resetBtn.addEventListener('click', clearBoard);
+
+	return { renderBoard };
+})()
+
+// render board on browser load
+displayController.renderBoard();
